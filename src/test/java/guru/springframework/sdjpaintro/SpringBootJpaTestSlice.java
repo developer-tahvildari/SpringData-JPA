5f840917d33edfb3af4d51219cc1d1d6370a9291
developer-tahvildari <developer.tahvildari@gmail.com>
2023-01-04 17:37:38 +0100

    add method order test

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -2,21 +2,28 @@ package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Created by Abdolrahim Tahvildari on 1/4/2023
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     private BookRepository bookRepository;
 
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         //given
@@ -29,5 +36,16 @@ public class SpringBootJpaTestSlice {
         //then
         long countAfter = bookRepository.count();
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        //when
+        long countBefore = bookRepository.count();
+
+        //then
+        assertThat(countBefore).isEqualTo(1);
+
     }
 }
